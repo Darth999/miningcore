8cfef617e75efcbf36f6404369d35e63b006d7bc
Sven Ruddies <operator@op-sys.de>
2025-05-09 09:06:19 +0200

    Compatibility

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJobManagerBase.cs
@@ -463,11 +463,16 @@ public abstract class BitcoinJobManagerBase<TJob> : JobManagerBase<TJob>
                 throw new PoolStartupException($"Init RPC failed: {string.Join(", ", errors.Select(y => y.Error.Message))}", poolConfig.Id);
         }
 
-        if(responses[2].Response["difficulty"]?["proof-of-work"] != null)
+        var proofOfWorkToken2 = responses[2].Response.SelectToken("difficulty.proof-of-work");
+
+        if(proofOfWorkToken2 != null)
         {
             responses[2].Response["difficulty"] = new JValue((double) responses[2].Response["difficulty"]["proof-of-work"]);
         }
-        if(responses[3].Response["proof-of-work"] != null)
+
+        var proofOfWorkToken3 = responses[3].Response.SelectToken("proof-of-work");
+
+        if(proofOfWorkToken3 != null)
         {
             proofOfWork = new JValue((double) responses[3].Response["proof-of-work"]);
         }
